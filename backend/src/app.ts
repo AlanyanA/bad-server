eedad072518ae75fb4ee0580eac024215b872a62
Арман Аланян <armanalanyan007@gmail.com>
2026-06-28 14:01:12 +0300

    fixes 14

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -55,9 +55,10 @@ app.use(errorHandler)
 const bootstrap = async () => {
     try {
         await mongoose.connect(DB_ADDRESS)
-        await app.listen(PORT, () => console.log('ok'))
+        app.listen(PORT, () => console.log('ok'))
     } catch (error) {
         console.error(error)
+        process.exit(1)
     }
 }
 
