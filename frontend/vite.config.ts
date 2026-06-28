6d2cca5708510694a718ab72df986d85c2a3f5de
Арман Аланян <armanalanyan007@gmail.com>
2026-06-28 10:16:58 +0300

    fixes 6

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,12 +20,11 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @use "./src/scss/variables" as *;
-          @use "./src/scss/mixins";
+          @use "/src/scss/_variables" as *;
+          @use "/src/scss/mixins" as mixins;
         `,
       },
-
-    }
+    },
   },
 
 })
